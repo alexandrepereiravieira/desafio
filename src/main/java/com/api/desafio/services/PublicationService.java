684481aa78c3352aa6ab68fed08fbd37d5b32c6b
AlexandrePereiraVieira <alexandrepereiravieira@gmail.com>
2022-09-15 00:18:45 -0300

    classe user ok classe publication ok Post register ok post login ok put editar publicação ok get retornar com os dados do publicação get listar publicação ok em ordem desc ou asc ok pelo ID faltando Delete ok

--- src/main/java/com/api/desafio/services/PublicationService.java
+++ src/main/java/com/api/desafio/services/PublicationService.java
@@ -2,9 +2,13 @@ package com.api.desafio.services;
 
 import com.api.desafio.models.PublicationModel;
 import com.api.desafio.repositories.PublicationRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PublicationService {
@@ -14,8 +18,29 @@ public class PublicationService {
     public PublicationService (PublicationRepository publicationRepository){
         this.publicationRepository = publicationRepository;
     }
+
+
     @Transactional
     public PublicationModel save(PublicationModel publicationModel) {
         return publicationRepository.save(publicationModel);
     }
+
+  /*  public Object findAllPublication() {
+            return publicationRepository.findAll();
+
+    }*/
+
+
+    public Page<PublicationModel> findAll(Pageable pageable) {
+        return publicationRepository.findAll(pageable);
+    }
+
+    public Optional<PublicationModel> findById(UUID id) {
+        return publicationRepository.findById(id);
+    }
+    @Transactional
+    public void delete(PublicationModel publicationModel) {
+        publicationRepository.delete(publicationModel);
+    }
+
 }
